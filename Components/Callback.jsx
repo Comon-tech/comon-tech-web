8244f21adfab59ac3e6afaa672bd375e7e7cae77
C-o-m-o-n <comon928@gmail.com>
2023-12-07 22:50:32 +0300

    pushing  to comon-tech

--- Components/Callback.jsx
+++ Components/Callback.jsx
@@ -14,7 +14,7 @@ const GithubAuthButton = () => {
       // You have the access token, you can now use it for API requests or store it as needed
       console.log('Access Token:', accessToken);
       // Optionally, you can redirect the user to another page or perform additional actions
-      window.location.href = '/web';
+      // window.location.href = '/dashboard';
     }
   }, []);
 
@@ -42,7 +42,7 @@ const GithubAuthButton = () => {
   return (
     <div className="flex flex-row bg-black gap-3 text-white p-4 border-[grey] border-2 rounded-2xl">
       <AiFillGithub size={23} />
-      <a href="https://github.com/login/oauth/authorize?scope=user&client_id=bd675e28f2391ed38be5&redirect_uri=http://localhost:3000">
+      <a href="https://github.com/login/oauth/authorize?scope=user&client_id=bd675e28f2391ed38be5&redirect_uri=http://localhost:3000/">
         Login with GitHub
       </a>
     </div>
